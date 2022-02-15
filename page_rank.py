7a266d373d84fc78c82d1c593e31672aa345697a
vtrain <vtrain@node0.group14-a2.ut-cs378-s22-pg0.utah.cloudlab.us>
2022-02-14 18:31:45 -0700

    On flat mapping part

--- page_rank.py
+++ page_rank.py
@@ -1,10 +1,13 @@
-import sys
 from pyspark.sql import SparkSession
-
+from pyspark.sql.functions import lit
+import sys
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 spark = SparkSession.builder.appName("Page_Ranking").getOrCreate()
-links = spark.read.text(input_file)
-llist = links.collect()
-for i in range(10):
-    print(llist[i])
+links = spark.read.csv(input_file, comment="#", sep=r'\t').toDF("Page", "Neighbor")
+ranks = links.select("Page").distinct().withColumn("Rank", lit(1))
+# links = links.rdd
+iterations = 1
+for i in range(iterations):
+    contribs = links.join(ranks, ['Page']).rdd.flatMap(lambda x: (x[0], x[1], x[2])).collect()
+    print(contribs)
