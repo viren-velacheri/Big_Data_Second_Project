9d435ff5a6db191dc66ec281fd4395adc47d896e
vtrain <vtrain@node0.group14-a2.ut-cs378-s22-pg0.utah.cloudlab.us>
2022-02-20 19:14:32 -0700

    Just modified script and coding files slightly and got output

---
+++ Task1/large_run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# this is for running page ranking algorithm on small dataset
+python3 large_dataset_page_rank.py $1 $2
---
+++ Task1/small_run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# this is for running page ranking algorithm on small dataset
+python3 page_rank.py $1 $2
+
