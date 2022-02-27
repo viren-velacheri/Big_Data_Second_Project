8da5208e2324c12acc102ac0530e368c86ce89e2
Pranav Akinepalli <pranav.akinepalli@gmail.com>
2022-02-27 00:27:21 -0600

    Updated comments in run scripts and readmes

--- Task4/small_run.sh
+++ Task4/small_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # this is for running page ranking algorithm on small dataset
+# assumes that the local spark directory is directly under /mydata/
 /mydata/spark-3.2.1-bin-hadoop3.2/bin/spark-submit small_dataset_page_rank.py $1 $2
 
